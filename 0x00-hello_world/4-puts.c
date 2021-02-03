292c339f1c41e8eac933c19000964e7582783d6a
BezK97 <bkifle97@gmail.com>
2021-02-03 14:40:38 +0000

    My first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
 /* To print string using puts() */
 int main(void)
 {
-	char *s = "Programming is like building a multilingual puzzle"
-
-	puts(s);
+	char My_msg[] = "\"Programming is like building a multilingual puzzle";
+	puts(My_msg);
 	return (0);
 }
