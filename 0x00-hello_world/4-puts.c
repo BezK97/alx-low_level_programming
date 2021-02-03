60d49b41a9bed3bd725e8395df09670e3e372505
BezK97 <bkifle97@gmail.com>
2021-02-03 15:10:55 +0000

    My first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-/* Description: print string using puts() */
+/**
+ * main - Entry point
+ *
+ * puts: print string
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char My_msg[] = "\"Programming is like building a multilingual puzzle";
