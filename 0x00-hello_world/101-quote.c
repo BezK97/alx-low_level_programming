20f38a6e4fbb7bffeb2c7cf868d6e21bdabdb38f
BezK97 <bkifle97@gmail.com>
2021-02-03 19:46:00 +0000

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <errno.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char My_msg[] = "and that piece of art is useful\" - Dora Korpar,
+2015-10-19\n";
+
+	perror(My_msg);
+	return (1);
+}
