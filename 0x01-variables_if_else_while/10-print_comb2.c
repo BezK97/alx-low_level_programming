455cc3ca1dfcb2b18092f1925f8aade1c188f193
BezK97 <bkifle97@gmail.com>
2021-02-04 19:44:55 +0000

    0 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2 = '0';
+
+	while (num1 <= '9')
+	{
+		while (num2 <= '9')
+		{
+			putchar(num1);
+			putchar(num2);
+
+			if (num2 != '9' || num1 != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
