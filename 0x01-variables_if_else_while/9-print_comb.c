ace70ef650ef6664afd4f78e7df8127c7bf8e21f
BezK97 <bkifle97@gmail.com>
2021-02-04 17:40:26 +0000

    Combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,16 @@ int main(void)
 {
 	int num = '0';
 
-	while (num <= '8')
+	while (num <='9')
 {
 	putchar(num);
+	if (num != 9)
+{
 	putchar(',');
 	putchar(' ');
+}
 	num++;
 }
-	putchar(num);
+	putchar('\n');
 	return (0);
 }
