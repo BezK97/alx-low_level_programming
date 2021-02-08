02e083e56009540dbcbf5cec1b96e1fd15a0ec8e
BezK97 <bkifle97@gmail.com>
2021-02-08 07:37:12 +0000

    Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,37 +1,25 @@
 #include <stdio.h>
-#include "_putchar.c"
+#include "function_declare.h"
 
 /**
  * main - Entry point
+ *
+ * _putchar: Calls function _putchar
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int count, msg;
-
-	for (count = 1; count <= 10; count++)
-	{
-		if (count == 1)
-			msg = _putchar('H');
-		else if (count == 2 || count == 8)
-			msg = _putchar('o');
-		else if (count == 3)
-			msg = _putchar('l');
-		else if (count == 4)
-			msg = _putchar('b');
-		else if (count == 5)
-			msg = _putchar('e');
-		else if (count == 6)
-			msg = _putchar('r');
-		else if (count == 7)
-			msg = _putchar('t');
-		else if (count == 9)
-			msg = _putchar('n');
-		else
-			msg = _putchar('\n');
-	}
-	putchar(msg);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchae('\n');
 	return (0);
 }
