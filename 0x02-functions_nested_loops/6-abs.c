396b6f0fc17dcf13025e82e4cb7708838742883c
BezK97 <bkifle97@gmail.com>
2021-02-08 11:26:09 +0000

    Compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _abs - compute absolute value
+ *
+ * @num: value to compute
+ *
+ * Return: num, the absolute value
+ */
+int _abs(int num)
+{
+	if (num >= 0)
+	{
+		return (num);
+	}
+	else
+	{
+		num = -num;
+		return (num);
+	}
+}
