021a905e41cd2cdaf3d04c9b77a6dfdf264faa6e
BezK97 <bkifle97@gmail.com>
2021-02-16 18:05:18 +0000

    Random

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Random number generator
+ *
+ * Return: number
+ */
+int main(void)
+{
+	int num;
+
+	while (num < 10000000)
+	{
+		num = (rand() % 99999999 + 1);
+	}
+	return (num);
+}
