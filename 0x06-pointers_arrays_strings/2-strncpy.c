7be23cd3bda2ac27d1e603e1757328218b372207
BezK97 <bkifle97@gmail.com>
2021-02-18 09:45:04 +0000

    Copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int len = 0;
 
-	while (len < n)
+	while (len < n && *(src + len) != '\0' )
 	{
-		if (*(src + len) == '\0')
-		{
-			break;
-		}
 		*(dest + len) = *(src + len);
 		len++;
 	}
