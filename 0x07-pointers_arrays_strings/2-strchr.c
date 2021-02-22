35aa27a91008ad60a07ad827bb353758f9f973e1
BezK97 <bkifle97@gmail.com>
2021-02-22 11:18:39 +0000

    First occurrence

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strchr - points to the first occurance of a character
+ *
+ * @s: string to check
+ * @c: character to point
+ *
+ * Return: s pointed to c character
+ */
+char *_strchr(char *s, char c)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		if (*(s + len) == c)
+			break;
+		len++;
+	}
+	return ((s + len));
+}
