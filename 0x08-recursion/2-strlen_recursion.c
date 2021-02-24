fb667c0ba124396fbd7fab2cdba081ea9c226aa3
BezK97 <bkifle97@gmail.com>
2021-02-24 17:23:49 +0000

    Length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,9 +9,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*(s + 1) == '\0')
-		return (1);
-	s++;
-	_strlen_recursion(s);
-	return (_strlen_recursion(s) + 1);
+	if (*s != '\0')
+		return (_strlen_recursion(s) + 1);
+	else
+		return (0);
 }
