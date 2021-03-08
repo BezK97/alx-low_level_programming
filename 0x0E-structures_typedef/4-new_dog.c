c7d84faf916a9278d4c3d3af330641439e81fa21
BezK97 <bkifle97@gmail.com>
2021-03-08 20:51:53 +0000

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,7 +46,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	mem->owner = malloc(sizeof(owner) + 1);
-	if (name == NULL)
+	if (mem->owner == NULL)
 	{
 		free(mem->name);
 		free(mem);
