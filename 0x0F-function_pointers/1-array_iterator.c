e92fa23f36936fafe4572d853c2daadb0869d458
BezK97 <77895681+BezK97@users.noreply.github.com>
2021-03-11 17:10:44 +0300

    Execute function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,21 @@
-#
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints arrays
+ * @array: Array to print
+ * @size: Size of array
+ * @action: Pointer to function
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+
+{
+	size_t x;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (x = 0; x < size; x++)
+		action(array[x]);
+}
