b60e68970ed914ceb7678dbb4009dea5715d0ba6
BezK97 <bkifle97@gmail.com>
2021-03-11 14:54:03 +0000

    Description

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all -entry point
+ *
+ * @n: number arguments
+ *
+ * Return: addition
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int num = 0;
+
+	va_list mylist;
+
+	va_start(mylist, n);
+
+	if (n == 0)
+		return (0);
+
+	for (num = 0; num < n; num++)
+	{
+		sum += va_arg(mylist, int);
+	}
+
+	va_end(mylist);
+
+	return (sum);
+}
