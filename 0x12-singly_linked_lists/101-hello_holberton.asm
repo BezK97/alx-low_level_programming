e6e63094896876b5680b49c393511614eaee7bd3
BezK97 <bkifle97@gmail.com>
2021-03-18 20:46:58 +0000

    Assembly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+	msg db "Hello, holberton", 0
+	fmt db "%s", 10, 0
+
+section .text
+	global main
+	extern printf
+
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
