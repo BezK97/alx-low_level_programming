d480487ceaf9ba6f573e5a36e537fb1a9bf95b07
BezK97 <bkifle97@gmail.com>
2021-03-22 22:24:54 +0000

    Free linked list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp, *tmp2 = *head;
 
-	while (tmp2 != NULL)
+	if (head != NULL)
 	{
-		tmp = (*tmp2).next;
-		free(tmp2);
-		tmp2 = tmp;
+		while (tmp2 != NULL)
+		{
+			tmp = (*tmp2).next;
+			free(tmp2);
+			tmp2 = tmp;
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
