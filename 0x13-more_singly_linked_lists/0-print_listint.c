9f790ae4eb2701ff161b3c7acf384acf43ad4316
BezK97 <bkifle97@gmail.com>
2021-03-22 08:51:30 +0000

    List linked integer

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 
 /**
+ * print_listint - function that prints all the elements of a listint_t list
  *
+ * @h: list to print
+ *
+ * Return: The number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
