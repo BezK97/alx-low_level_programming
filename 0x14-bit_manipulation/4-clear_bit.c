ef02febcb6f1b95355603a24267fe63aff48e6b9
BezK97 <bkifle97@gmail.com>
2021-03-25 20:25:45 +0000

    Set bit to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - function that sets the value of a bit to 0 at a given index
+ * @n: number to index
+ * @index: indext to binary
+ *
+ * Return: 1 if success, -1 if not
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+        unsigned long int x = 1;
+
+        if (index > 32 || n == NULL)
+                return (-1);
+        *n = *n & ~(x << index);
+        return (1);
+}
