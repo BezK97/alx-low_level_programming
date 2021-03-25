9bdc764a60423c257ed91703ff4585c4cc700ae0
BezK97 <bkifle97@gmail.com>
2021-03-25 20:39:08 +0000

    Set bit to 0 at index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,10 +9,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-        unsigned long int x = 1;
+	unsigned long int x = 1;
 
-        if (index > 32 || n == '\0')
-                return (-1);
-        *n = *n & ~(x << index);
-        return (1);
+	if (index > 32 || n == '\0')
+		return (-1);
+	*n = *n & ~(x << index);
+	return (1);
 }
