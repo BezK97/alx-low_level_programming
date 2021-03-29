3e35ecb34e48a451a2c1812566215fbb7b49f9b1
BezK97 <bkifle97@gmail.com>
2021-03-29 17:52:01 +0000

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,9 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_TRUNC | O_RDWR);
 	if (file == -1)
 		return (-1);
-	for (len = 0; text_content[len]; len++)
+	for (len = 0; text_content[len] != NULL; len++)
 		;
-	if (len == 0)
+	if (text_content == NULL)
 		write(file, "", len);
 	else
 		write(file, text_content, len);
