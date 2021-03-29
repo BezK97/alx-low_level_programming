af94e6a983ca374ebdfadf8933ae05de517a8b7b
BezK97 <bkifle97@gmail.com>
2021-03-29 20:40:05 +0000

    Copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,8 @@
  *
  * @argc: number of argument passed
  * @argv: argument passed
+ *
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
@@ -12,10 +14,7 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
@@ -23,34 +22,28 @@ int main(int argc, char *argv[])
 			"Error: Can't read from file %s\n", argv[0]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_CREAT | O_APPEND | O_WRONLY, 0644);
+	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 		exit(99);
 	}
-	if (read(file_from, buffer, 1024) == - 1)
+	if (read(file_from, buffer, 1024) == -1)
 	{
 		dprintf(STDERR_FILENO,
-                        "Error: Can't read from file %s\n", argv[0]);
+			"Error: Can't read from file %s\n", argv[0]);
 		exit(98);
 	}
 	for (; buffer[len] != '\0'; len++)
 		;
-	if (write(file_to, buffer, len) == - 1)
+	if (write(file_to, buffer, len) ==  -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 		exit(99);
 	}
 	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %d\n", file_to);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", file_to), exit(100);
 	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %d\n", file_from);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", file_from), exit(100);
 	return (0);
 }
